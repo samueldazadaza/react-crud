bf3e94f96a91d6aa972ebd0bb787b6e9bc832812
sam <samueldazadaza@gmail.com>
2022-03-22 19:52:59 -0500

    24-formulario

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Listadonombres from './components/Listadonombres'
+
+function App() {
+  return (
+    <div className="container">
+      <Listadonombres/>
+    </div>
+  );
+}
+
+export default App;
--- src/components/Listadonombres.jsx
+++ src/components/Listadonombres.jsx
@@ -1,8 +1,22 @@
-import React, [useEffect] from 'react'
+import React, {useState} from 'react'
 
 const Listadonombres = () => {
   return (
-    <div>Listadonombres</div>
+    <div>
+        <h2>Aplicacion crud basica</h2>
+          <div className="row">
+              <div className="col">
+                <h2>Listado de nombres</h2>
+              </div>
+              <div className="col">
+                <h2>Formulario para añdir nombres</h2>
+					<form className='form-group'>
+						<input className='form-control mb-3' type="text" placeholder='Introduce el nombre' />
+						<input className='btn btn-info btn-block' type="submit" value='Registrar nombre' />
+					</form>
+              </div>
+          </div>
+    </div>
   )
 }
 
