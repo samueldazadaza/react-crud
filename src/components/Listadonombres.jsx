e1ebfc86a4e9ea7c6ccb06dcc24f5016a81da646
sam <samueldazadaza@gmail.com>
2022-04-13 20:03:55 -0500

    29. comprobacion de errores

--- src/components/Listadonombres.jsx
+++ src/components/Listadonombres.jsx
@@ -7,15 +7,21 @@ const Listadonombres = () => {
 	const [listanombres, setListaNombres] = useState([])
 	const [modoEdicion, setModoEdicion] = useState(false)
 	const [id, setId] = useState('')
+	const [error, setError] = useState(null)
 
 	const addNombre = (e) => {
 		e.preventDefault()
+		if(!nombre.trim()){
+			setError('El campo nombre esta vacio')
+			return
+		}
 		const nuevoNombre = {
 			id: uniqid(),
 			tituloNombre: nombre
 		}
 		setListaNombres([...listanombres, nuevoNombre])
 		setNombre('')
+		setError(null)
 	}
 	const deleteNombre = (id) => {
 		const nuevaArray = listanombres.filter( item => item.id !== id)
@@ -31,6 +37,8 @@ const Listadonombres = () => {
 		const NuevoArray = listanombres
 		.map(item => item.id === id ? {id:id, tituloNombre:nombre} : item)
 		setListaNombres(NuevoArray)
+		setModoEdicion(false)
+		setNombre('')
 	}
 
 
@@ -81,6 +89,16 @@ const Listadonombres = () => {
 									value={modoEdicion ? 'EDITAR NOMBRE' : 'REGISTRAR NOMBRE'}
 								/>
 							</form>
+							{
+								error != null ? (
+									<div className='alert alert-danger'>
+										{error}
+									</div>
+								) :
+								(
+									<div></div>
+								)
+							}
 					</div>
 				</div>
 		</div>
